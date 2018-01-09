b288615f0f6b9373cc68d877b9c2c1a0c4b64a73
Disa <dis@mail.rotf.pl>
2018-01-09 02:48:39 +0100

    init nn

---
+++ NeuralNetwork/Neuron.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class Neuron
+    {
+        private double bias;
+        private Dictionary<Neuron, double> top;
+        private double delta;
+        private double value;
+        public Neuron(){
+            
+        }
+        public double Val
+        {
+            get { return value; }
+        }
+        public void calculate()
+        {
+            double sum = bias;
+            foreach (KeyValuePair <Neuron, double> parent in top)
+            {
+                sum += parent.Key.Val * parent.Value;
+            }
+            value = sigmoid(sum);
+        }
+        public double sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+    }
+}
