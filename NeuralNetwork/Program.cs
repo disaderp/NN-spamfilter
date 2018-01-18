fd07155ce4691e1ef59c4b418ad201533a3cec0e
Disa <dis@mail.rotf.pl>
2018-01-18 23:26:09 +0100

    training+validation

--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -15,43 +15,52 @@ namespace NeuralNetwork
             int hidden = 4;//dla 3 nie osiaga najwyzszego wyniku, 6 duzo dluzej sie uczy
             Shared.initialize();
             Console.WriteLine("NeuralNetwork - hiddenLayer=" + hidden + "; LearningRate=" + Neuron.LEARNINGRATE + "; epochDecrease=" + Network.epochdecrease);
-            Dictionary<List<double>, bool> emails = new Dictionary<List<double>, bool>();
+            Dictionary<List<double>, bool> train = new Dictionary<List<double>, bool>();
+            Dictionary<List<double>, bool> validate = new Dictionary<List<double>, bool>();
             List<List<double>> templist;
             bool[] boollist;
-            string[] json = File.ReadAllText("validation.txt").Split('X');
+            string[] json = File.ReadAllText("learning.txt").Split('X');
             templist = JsonConvert.DeserializeObject<List<List<double>>>(json[0]);
             boollist = JsonConvert.DeserializeObject<bool[]>(json[1]);
             for (int i = 0; i< boollist.Count(); i++)
             {
-                emails.Add(templist[i], boollist[i]);
+                train.Add(templist[i], boollist[i]);
+            }
+
+            json = File.ReadAllText("validation.txt").Split('X');
+            templist = JsonConvert.DeserializeObject<List<List<double>>>(json[0]);
+            boollist = JsonConvert.DeserializeObject<bool[]>(json[1]);
+            for (int i = 0; i < boollist.Count(); i++)
+            {
+                validate.Add(templist[i], boollist[i]);
             }
             templist = null;
             boollist = null;
 
-            Network net = new NeuralNetwork.Network(emails.ElementAt(0).Key.Count(), hidden);
+            Network net = new NeuralNetwork.Network(train.ElementAt(0).Key.Count(), hidden);
             int ok = 0;
-            for (int j = 0; j < 100; j++)
+            for (int j = 0; j < 20; j++)
             {
                 ok = 0;
-                for (int i = 0; i < emails.Count(); i++)
+                for (int i = 0; i < train.Count(); i++)
                 {
-                    net.updateInputs(emails.ElementAt(i).Key);
+                    net.updateInputs(train.ElementAt(i).Key);
                     bool det = false; if (net.getOutput() > 0.5) det = true;
-                    if (det == emails.ElementAt(i).Value) ok++;
-                    net.calcErr(boolToDouble(emails.ElementAt(i).Value));
+                    if (det == train.ElementAt(i).Value) ok++;
+                    net.calcErr(boolToDouble(train.ElementAt(i).Value));
                 }
-                Console.WriteLine("Epoch " + j + " Detection ratio: " + (float)(ok * 100 / emails.Count()) + "%");
+                Console.WriteLine("Epoch " + j + " Training data detection ratio: " + (float)(ok * 100 / train.Count()) + "%");
                 net.nextEpoch();
             }
             ok = 0;
-            for(int i= 0;i < emails.Count(); i++)
+            for(int i= 0;i < validate.Count(); i++)
             {
-                net.updateInputs(emails.ElementAt(i).Key);
+                net.updateInputs(validate.ElementAt(i).Key);
                 bool det = false; if (net.getOutput() > 0.5) det = true;
-                if (det == emails.ElementAt(i).Value) ok++;
+                if (det == validate.ElementAt(i).Value) ok++;
             }
-            Console.WriteLine("Detection ratio: " + (float)(ok * 100 / emails.Count()) + "%");
-            Console.ReadKey();
+            Console.WriteLine("Validation data detection ratio: " + (float)(ok * 100 / validate.Count()) + "%");
+            Console.ReadLine();
         }
         static double boolToDouble(bool val)
         {
